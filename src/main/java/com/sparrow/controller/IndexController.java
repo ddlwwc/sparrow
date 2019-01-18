fde8c16667536d145d2cfe590aba026046d9439e
wancheng <394950814@qq.com>
2019-01-18 14:24:25 +0800

    M.首页

--- src/main/java/com/sparrow/controller/IndexController.java
+++ src/main/java/com/sparrow/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.sparrow.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by wancheng on 2019/1/16.
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
     @RequestMapping("/")
-    public String index()  {
-        return "forward:index.html";
+    public String index(ModelAndView model)  {
+        return "index";
     }
 }
